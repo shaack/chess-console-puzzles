1aa23ae6db91135fa2d59fd66f2ea1db19250130
shaack <shaack@gmail.com>
2021-11-22 01:09:45 +0100

    renamed to cm-puzzle-player and cleanup

--- postinstall.js
+++ postinstall.js
@@ -7,5 +7,7 @@ const LibraryManager = require("cm-web-modules/src/LibraryManager.js")
 const manager = new LibraryManager(__dirname)
 
 manager.addPackage("cm-web-modules")
+manager.addPackage("chess.mjs")
 manager.addPackage("cm-chess")
+manager.addPackage("cm-pgn")
 manager.addPackage("cm-chessboard")
